b1f232333c6a198415651dd3d4541524dca4e0a3
webTOdev <nusratst.cse@gmail.com>
2013-10-04 00:53:06 +0600

    Angles Sorted

---
+++ graphutility.cpp
@@ -0,0 +1,41 @@
+/*
+ * graphutility.cpp
+ *
+ *  Created on: Oct 3, 2013
+ *      Author: nut
+ */
+
+#include "graphutility.h"
+#include <cmath>
+
+double vectorsAngle( double x, double y, double basex, double basey)
+{
+	// Convert input point x & y to be vectors relative to base point
+	double x2 = double(x - basex);
+	double y2 = double(y - basey);
+
+	// Hard code scan line to point right:
+	double x1 = sqrt( x2*x2 + y2*y2 ); // make it with ratio?
+	double y1 = 0.0;
+
+	//cout << "x1: " << x1 << " - y1: " << y1 << endl;
+	//cout << "x2: " << x2 << " - y2: " << y2 << endl;
+
+	double stuff = (  (x1*x2)+(y1*y2)   ) / (  sqrt(x1*x1+y1*y1)  *  sqrt(x2*x2+y2*y2) );
+	//cout << "Stuff: " << stuff << endl;
+
+	// Calculate angle:
+	double result = acos( stuff );
+	//cout << "Result: " << result << endl;
+
+	// Now add PI if below middle line:
+	if( y >= basey )
+		result = 2*M_PI - result;
+
+	//cout << "Result: " << result*180/M_PI << " degrees" << endl;
+
+	return result;
+}
+
+
+
---
+++ graphutility.h
@@ -0,0 +1,18 @@
+/*
+ * graphutility.h
+ *
+ *  Created on: Oct 3, 2013
+ *      Author: nut
+ */
+
+#ifndef GRAPHUTILITY_H_
+#define GRAPHUTILITY_H_
+
+
+//-------------------------------------------------------------------------------
+//  Calculate Angle Btw 2 Vectors
+//Turning the points in to vector by x=basex-x and y = basey-y and then angle = arctan(y/x);
+//-------------------------------------------------------------------------------
+double vectorsAngle( double x, double y, double basex, double basey);
+
+#endif /* GRAPHUTILITY_H_ */
